d88ed3519dda9600272a88defe449c9bf84aecd3
Satvir Hundal <stormschool1@gmail.com>
2026-08-03 16:03:28 -0700

    Add: permutation-string - submission-10

---
+++ Data Structures & Algorithms/permutation-string/submission-10.py
@@ -0,0 +1,44 @@
+class Solution:
+    from collections import defaultdict
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        let = defaultdict(int)
+
+        cur = defaultdict(int)
+
+        for letter in s1:
+
+            cur[letter] += 1
+
+        l = 0
+        r = 1
+
+        while r <= len(s2):
+
+            string = s2[l:r]
+
+            print(len(string), len(s1))
+
+            if len(string) == len(s1):
+
+                for letter in string:
+
+                    let[letter] += 1
+
+                if let == cur:
+
+                    return True
+
+                else:
+
+                    let = defaultdict(int)
+                    l += 1
+
+            r += 1
+
+        return False
+
+
+
+
+        
