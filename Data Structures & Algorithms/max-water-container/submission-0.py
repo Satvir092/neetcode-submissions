09edb562dce7dc1ffdddbb753077408e64ae911e
Satvir Hundal <stormschool1@gmail.com>
2026-09-08 18:33:42 -0700

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        
+        l = 0
+        r = len(heights) - 1
+
+        maxi = 0
+
+        while l < r:
+
+            area = min(heights[l], heights[r]) * (r - l)
+
+            maxi = max(maxi, area)
+
+            if heights[l] > heights[r]:
+
+                r -= 1
+
+            else:
+
+                l += 1
+
+        return maxi
